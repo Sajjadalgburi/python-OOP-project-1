ad962f89e3d26f51e276980e3712c26e27fb2e7e
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-10-31 19:33:47 -0600

    created the get balance and depost methods

--- bank_accounts.py
+++ bank_accounts.py
@@ -3,3 +3,17 @@ class BankAccount:
         self.balance = initialAmount
         self.name = accName
         print(f"\nAccount '{self.name}' created. \n Balance = ${self.balance:.2f}")
+
+    def getBalance(self):
+        print(f"Your balance is ${self.balance:.2f}")
+
+    def deposit(self, amount):
+        if not isinstance(amount, (int, float)):
+            print("\nMust pass a valid number")
+        elif amount < 15:
+            print("\nMinimum deposit amount is $15")
+        else:
+            self.balance += amount
+            print(
+                f"\nYou have successfully made a deposit.\nYour new balance is {self.balance}"
+            )
